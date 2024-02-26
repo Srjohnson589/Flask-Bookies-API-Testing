3133158d2b208ce66a68d2c0522e39e3c4d58ea1
Srjohnson589 <srjohnson4586@gmail.com>
2024-02-25 20:35:51 -0500

    started routes

--- app/blueprints/bookie_api/routes.py
+++ app/blueprints/bookie_api/routes.py
@@ -4,7 +4,7 @@ from flask import request, jsonify
 
 from app.models import User, Book, db
 
-# Create a User
+# CREATE
 @bookie_api.post('/create_user')
 def create_user():
     '''
@@ -23,3 +23,45 @@ def create_user():
         'status':'ok',
         'message':'User successfully created'
     })
+
+#CREATE & UPDATE
+@bookie_api.post('/add_current_book')
+def add_current_book():
+    '''
+    -if book not in database, first create book
+    -remove book from current users other lists
+    -add to current list
+    
+    '''
+
+
+@bookie_api.post('/add_read_book')
+def add_read_book():
+    '''
+    -if book not in database, first create book
+    -remove book from current users other lists
+    -add to current list
+    
+    '''
+
+@bookie_api.post('/add_to_read_book')
+def add__to_read_book():
+    '''
+    -if book not in database, first create book
+    -remove book from current users other lists
+    -add to current list
+    
+    '''
+
+# READ - Get all user reading lists
+
+@bookie_api.get('/user_reading_lists')
+def user_reading_lists():
+    '''
+    -takes in a user
+    -returns json with current, read, and to read lists
+    
+    '''
+
+# DELETE - delete a book off a list
+    
